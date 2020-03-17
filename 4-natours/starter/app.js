a5526f3519fde20b431a32bdec27dbc798d02673
achia222 <achia222@gmail.com>
2020-03-17 20:21:54 +0200

    first post

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -3,6 +3,8 @@ const app = express();
 
 const fs = require('fs');
 
+app.use(express.json()) //middleman to be able to req.body
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`listning on port ${port}...`);
@@ -16,3 +18,13 @@ const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours-simple.json'));
 app.get("/api/v1/tours", (req, res) => {
     res.status(200).json(tours);
 });
+
+app.post("/api/v1/tours", (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour);
+    fs.writeFile('./dev-data/data/tours-simple.json', JSON.stringify(tours), err => {
+        res.status(201).json(newTour);
+    })
+})
